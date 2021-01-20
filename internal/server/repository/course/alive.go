ac0500c8ef8785a2f655c0c5e5b758f5218603b0
dexterli <dexterli@xiaoe-tech.com>
2021-01-20 09:51:37 +0800

    清晰度顺序

--- internal/server/repository/course/alive.go
+++ internal/server/repository/course/alive.go
@@ -426,6 +426,12 @@ func (a *AliveInfo) GetAliveLiveUrl(aliveType uint8, agentType int, UserId, play
 		liveUrl.PcAliveVideoMoreSharpness = make([]map[string]interface{}, len(supportSharpness))
 		i := 0
 		for k, v := range supportSharpness {
+			switch k {
+			case "fluent":
+				i = 0
+			case "default":
+				i = 1
+			}
 			currentSharpnessUrl := a.getPlayUrlBySharpness(k, playUrls[2], channelId)
 			liveUrl.AliveVideoMoreSharpness[i] = map[string]interface{}{
 				"definition_name": v,
@@ -440,7 +446,6 @@ func (a *AliveInfo) GetAliveLiveUrl(aliveType uint8, agentType int, UserId, play
 				"url":             currentSharpnessUrl,
 				"encrypt":         "",
 			}
-			i++
 		}
 
 		// 快直播O端名单目录
