ff8bbfb0abefdaec412472b192ce39c5f74c1344
dexterli <dexterli@xiaoe-tech.com>
2021-01-26 11:42:16 +0800

    fix

--- service/available.go
+++ service/available.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"abs/pkg/app"
-	"abs/pkg/enums"
 	e "abs/pkg/enums"
 	"abs/pkg/logging"
 	"abs/pkg/util"
@@ -163,7 +162,7 @@ func (ava *AvailableService) IsResourceAccess(resourceId string, filterFree bool
 		logging.Error(err)
 		return false, err
 	}
-	if responseMap.Code != enums.SUCCESS {
+	if responseMap.Code != e.SUCCESS {
 		return false, errors.New(fmt.Sprintf("请求权益【内部课程和加密】信息错误：%s", responseMap.Msg))
 	}
 	data := responseMap.Data.(map[string]interface{})
