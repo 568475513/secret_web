80314ef596f66545b8681df7bdfa70bccf73e1a8
kevinYang <871518625@qq.com>
2021-03-31 15:55:58 +0800

    dexter说这里改为true

--- internal/server/repository/user/user_power_business.go
+++ internal/server/repository/user/user_power_business.go
@@ -44,7 +44,7 @@ func (upb *userPowerBusiness) IsHaveSpecialColumnPower(productId string) (string
 
 // 内部课程权限判断
 func (upb *userPowerBusiness) IsInsideAliveAccess(resouceId string) (bool, error) {
-	return upb.service.IsResourceAccess(resouceId, false, 0)
+	return upb.service.IsResourceAccess(resouceId, true, 0)
 }
 
 // 加密课程权益判断
