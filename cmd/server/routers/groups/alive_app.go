e2a80836bb3b8d57a372e38280e75df5c4d10329
kevinYoung <871518625@qq.com>
2021-04-30 13:08:35 +0800

    改下app接口的路由前缀

--- cmd/server/routers/groups/alive_app.go
+++ cmd/server/routers/groups/alive_app.go
@@ -10,7 +10,7 @@ import (
 
 func AliveAppRouter(Router *gin.RouterGroup) {
 	// app写在abs_go的路由都加上v2
-	apiRouterV2 := Router.Group("_alive/api/v2")
+	apiRouterV2 := Router.Group("_alive/api-v2")
 	//根据直播开始时间获取用户已订阅的直播课程列表
 	apiRouterV2.GET("get.alive.list.by.date", v2.GetSubscribeAliveListByDate)
 	//根据直播开始间获取用户已订阅的直播课程数量
