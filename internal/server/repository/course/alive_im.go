0142a687a2aa33c53f1dcf434ab2ea2ee5c45495
saijunchen <saijunchen>
2021-06-28 16:48:49 +0800

    fix im12

--- internal/server/repository/course/alive_im.go
+++ internal/server/repository/course/alive_im.go
@@ -35,6 +35,7 @@ func (a *AliveInfo) GetAliveRommId(a2 *alive.Alive) string {
 	}
 	defer redisConn.Close()
 	room_id := a2.RoomId
+	logging.Info(room_id)
 	imGroupActiveCacheKey := fmt.Sprintf(imGroupActive, a.AliveId)
 	rid, _ := redis.String(redisConn.Do("get", imGroupActiveCacheKey))
 	if rid != "" {
@@ -49,21 +50,28 @@ func (a *AliveInfo) GetAliveRommId(a2 *alive.Alive) string {
 		logging.Error(err)
 		return room_id
 	}
+	logging.Info(AliveImMiddler)
 	var newRoomId string
 	if AliveImMiddler.NewRoomId != "" {
 		newRoomId = AliveImMiddler.NewRoomId
 	} else {
 		newRoomId = a.getRandRoomId(10)
 	}
+	logging.Info(newRoomId)
 	res := a.hitJudgeActive(redisConn, newRoomId)
+	logging.Info(res)
 	if res {
 		err = alive.UpdateTAliveRommId(a.AppId, a.AliveId, newRoomId)
+		logging.Info(err)
 		if err != nil {
 			logging.Error(err)
+			return room_id
 		}
+
 		err = alive.UpdateForbidRoomId(a.AppId, room_id, newRoomId)
 		if err != nil {
 			logging.Error(err)
+			return room_id
 		}
 		aim := alive.AliveImMiddler{
 			AppId:     a.AppId,
@@ -72,9 +80,12 @@ func (a *AliveInfo) GetAliveRommId(a2 *alive.Alive) string {
 			NewRoomId: newRoomId,
 		}
 		err = alive.InsertImMiddle(aim)
+		logging.Info(aim)
 		if err != nil {
 			logging.Error(err)
+			return room_id
 		}
+		return newRoomId
 	}
 	return room_id
 }
@@ -89,14 +100,17 @@ func (a *AliveInfo) hitJudgeActive(redisConn redis.Conn, room_id string) bool {
 	}
 
 	hitImActiveCacheKey := fmt.Sprintf(hitImActive, a.AliveId[len(a.AliveId)-1:])
+	logging.Info(hitImActiveCacheKey)
 	zScoreValue, _ := redisConn.Do("zscore", hitImActiveCacheKey, a.AliveId)
 	expire := 86400 - (time.Now().Unix()+8*3600)%86400
+	logging.Info(zScoreValue)
 	if zScoreValue != nil {
 		redisConn.Do("setex", imGroupActiveCacheKey, expire, room_id)
 		redisConn.Do("zadd", hitImActiveCacheKey, time.Now().Unix(), a.AliveId)
 		return false
 	}
 	res := createGroup(room_id)
+	logging.Info(res)
 	if res {
 		redisConn.Do("setex", imGroupActiveCacheKey, expire, room_id)
 		redisConn.Do("zadd", hitImActiveCacheKey, time.Now().Unix(), a.AliveId)
@@ -132,6 +146,7 @@ func createGroup(GroupId string) bool {
 	request.SetParams(requestDataJson)
 	request.SetTimeout(1000 * time.Millisecond)
 	err := request.ToJSON(&responseMap)
+	logging.Info(responseMap)
 	ErrprCodes := map[int]string{
 		0:     "无错误。",
 		10021: "群组 ID 已被使用，请选择其他的群组 ID。",
