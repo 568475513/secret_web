4dc4b0a43304cd24ad1ac388225f7e08649c813b
xiayuping <darcyxia@xiaoe-tech.com>
2021-11-26 20:52:20 +0800

    token

--- pkg/util/utilphp.go
+++ pkg/util/utilphp.go
@@ -63,7 +63,7 @@ type ContentParam struct {
 	ShareFrom    string `json:"share_from"`
 	ExtraData    int    `json:"extra_data"`
 	WebAlive     int    `json:"web_alive"`
-	Token        string `json:"Token string"`
+	Token        string `json:"token "`
 }
 
 // 老的跳转拼接方法
