0690e7518359e6c82c30f54fcba99c621b882512
qiushengchen <qiushengchen@xiaoe-tech.com>
2021-12-16 16:31:31 +0800

    feat：热度开关

--- internal/server/repository/course/business_baseinfo.go
+++ internal/server/repository/course/business_baseinfo.go
@@ -284,8 +284,8 @@ func (b *BaseInfo) GetAliveConfInfo(baseConf *service.AppBaseConf, aliveModule *
 	aliveConf["alive_mode"] = aliveModule.AliveMode
 	aliveConf["is_picture_on"] = aliveModule.IsPictureOn
 	aliveConf["is_audit_first_on"] = aliveModule.IsAuditFirstOn
-	aliveConf["is_online_on"] = aliveModule.isOnlineOn
-	aliveConf["is_heat_on"] = aliveModule.isHeatOn
+	aliveConf["is_online_on"] = aliveModule.IsOnlineOn
+	aliveConf["is_heat_on"] = aliveModule.IsHeatOn
 	aliveConf["is_coupon_on"] = aliveModule.IsCouponOn
 	aliveConf["is_card_on"] = aliveModule.IsCardOn
 	aliveConf["is_prize_on"] = aliveModule.IsPrizeOn
