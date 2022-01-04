c4715fbccee393b0eb15f34e61c234bfee63dc69
zeqinzheng <zeqinzheng@xiaoe-tech.com>
2022-01-04 21:45:36 +0800

    新增resoure_type字段

--- service/camp.go
+++ service/camp.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"abs/pkg/logging"
 	"errors"
 	"fmt"
 	"os"
@@ -97,7 +96,6 @@ func (t *CampService) GetCampTermInfoV2(ids, fields []string) ([]map[string]inte
 	request.SetHeader("Content-Type", "application/json")
 	request.SetTimeout(timeoutTerm * time.Millisecond)
 	err = request.ToJSON(&result)
-	logging.Info(fmt.Sprintf("训练营数据terms:%v", result))
 
 	if err != nil {
 		return terms, err
