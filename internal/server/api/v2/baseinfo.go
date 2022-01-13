c6de0ffb11f0dcb6b744deeeb6e7349dd58e65ee
kuboliu <kuboliu@xiaoe-tech.com>
2022-01-13 11:23:04 +0800

    更新一下

--- internal/server/api/v2/baseinfo.go
+++ internal/server/api/v2/baseinfo.go
@@ -271,7 +271,12 @@ func GetBaseInfo(c *gin.Context) {
 		eCourseAvailableParams, _ = availableService.IsECourseAvailable(eCourseAvailable)
 		// 如果有结果的话， 权益就直接使用鹅课程的哦
 		if eCourseAvailableParams != nil {
-			availableInfo["available"] = eCourseAvailableParams.(map[string]interface{})["is_permission"]
+			if eCourseAvailableParams.(map[string]interface{})["is_permission"].(int) == 0 {
+				availableInfo["available"] = false
+			} else {
+				availableInfo["available"] = true
+			}
+
 		}
 
 	}
