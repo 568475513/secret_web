2967cd551c9488511aa94266b3e3dd6579e60f2b
qiushengchen <qiushengchen@xiaoe-tech.com>
2022-02-09 10:18:46 +0800

    fix

--- internal/server/repository/course/business_baseinfo.go
+++ internal/server/repository/course/business_baseinfo.go
@@ -473,7 +473,7 @@ func (b *BaseInfo) GetAliveLiveUrl(agentType, version, enableWebRtc int, UserId 
 
 	// 防盗链 start
 	isEncryptGrayBool := redis_gray.InGrayShop("alive_encrypt_gray", b.AliveRep.AppId)
-	if !isEncryptGrayBool && ua != "" {
+	if isEncryptGrayBool && ua != "" {
 		supportRefer := true
 		if strings.Contains(ua, "android") || strings.Contains(ua, "adr") {
 			if !(strings.Contains(ua, "tbs") || strings.Contains(ua, "xweb")) {
