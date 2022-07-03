5f5d0c8ea35d3ae338f3dc2604c5b1d03ba614a8
nickzhou <nickzhou@xiaoe-tech.com>
2022-07-03 23:58:48 +0800

    1

--- internal/server/api/secret/user_info.go
+++ internal/server/api/secret/user_info.go
@@ -151,6 +151,7 @@ func GetPreventList(c *gin.Context) {
 	}
 	s := map[int][]map[string]string{
 		1: {
+			//////////////////////////////////////////间谍开始////////////////////////////////////////
 			0: {
 				"AppName": "mSpy",
 				"AppType": "间谍软件",
@@ -164,39 +165,193 @@ func GetPreventList(c *gin.Context) {
 				"AppLink": "",
 			},
 			2: {
-				"AppName": "EasySpyApp",
+				"AppName": "Spyera",
 				"AppType": "间谍软件",
 				"AppRank": "3",
 				"AppLink": "",
 			},
 			3: {
-				"AppName": "麦苗守护",
+				"AppName": "FlexiSPY",
 				"AppType": "间谍软件",
 				"AppRank": "4",
 				"AppLink": "",
 			},
+			4: {
+				"AppName": "Qustodio",
+				"AppType": "间谍软件",
+				"AppRank": "5",
+				"AppLink": "",
+			},
+			5: {
+				"AppName": "SpyBubble",
+				"AppType": "间谍软件",
+				"AppRank": "6",
+				"AppLink": "",
+			},
+			6: {
+				"AppName": "TheWiSpy",
+				"AppType": "间谍软件",
+				"AppRank": "7",
+				"AppLink": "",
+			},
+			7: {
+				"AppName": "Spyic",
+				"AppType": "间谍软件",
+				"AppRank": "8",
+				"AppLink": "",
+			},
+			8: {
+				"AppName": "wondershare",
+				"AppType": "间谍软件",
+				"AppRank": "9",
+				"AppLink": "",
+			},
+			9: {
+				"AppName": "MobiStealth",
+				"AppType": "间谍软件",
+				"AppRank": "10",
+				"AppLink": "",
+			},
+			10: {
+				"AppName": "AppMia",
+				"AppType": "间谍软件",
+				"AppRank": "11",
+				"AppLink": "",
+			},
+			11: {
+				"AppName": "ESET Parental Control",
+				"AppType": "间谍软件",
+				"AppRank": "12",
+				"AppLink": "",
+			},
+			12: {
+				"AppName": "Highster Mobile",
+				"AppType": "间谍软件",
+				"AppRank": "13",
+				"AppLink": "",
+			},
+			13: {
+				"AppName": "Spyzie",
+				"AppType": "间谍软件",
+				"AppRank": "14",
+				"AppLink": "",
+			},
+			14: {
+				"AppName": "Notron Family",
+				"AppType": "间谍软件",
+				"AppRank": "15",
+				"AppLink": "",
+			},
+			15: {
+				"AppName": "EasySpyApp",
+				"AppType": "间谍软件",
+				"AppRank": "16",
+				"AppLink": "",
+			},
+			////////////////////////////////////////间谍结束////////////////////////////////////////
 		},
 		2: {
+			////////////////////////////////////////监控员工软件开始////////////////////////////////////////
 			0: {
-				"AppName": "HubStaff",
-				"AppType": "监控员工软件",
+				"AppName": "Scalefusion",
+				"AppType": "企业监控软件",
 				"AppRank": "1",
 				"AppLink": "",
 			},
 			1: {
-				"AppName": "Atto-Work",
-				"AppType": "监控员工软件",
+				"AppName": "HubStaff",
+				"AppType": "企业监控软件",
 				"AppRank": "2",
 				"AppLink": "",
 			},
-			3: {
-				"AppName": "OnTheClock",
-				"AppType": "监控员工软件",
+			2: {
+				"AppName": "Workfolio",
+				"AppType": "企业监控软件",
 				"AppRank": "3",
 				"AppLink": "",
 			},
+			3: {
+				"AppName": "Atto-Work",
+				"AppType": "企业监控软件",
+				"AppRank": "4",
+				"AppLink": "",
+			},
+			4: {
+				"AppName": "OnTheClock",
+				"AppType": "企业监控软件",
+				"AppRank": "5",
+				"AppLink": "",
+			},
+			5: {
+				"AppName": "The Team Tracker",
+				"AppType": "企业监控软件",
+				"AppRank": "6",
+				"AppLink": "",
+			},
+			6: {
+				"AppName": "QuickBooks",
+				"AppType": "企业监控软件",
+				"AppRank": "7",
+				"AppLink": "",
+			},
+			7: {
+				"AppName": "Homebase",
+				"AppType": "企业监控软件",
+				"AppRank": "8",
+				"AppLink": "",
+			},
+			8: {
+				"AppName": "Employee Tracker Lite",
+				"AppType": "企业监控软件",
+				"AppRank": "9",
+				"AppLink": "",
+			},
+			9: {
+				"AppName": "ezClocker-Employee",
+				"AppType": "企业监控软件",
+				"AppRank": "10",
+				"AppLink": "",
+			},
+			10: {
+				"AppName": "Justworks Hours",
+				"AppType": "企业监控软件",
+				"AppRank": "11",
+				"AppLink": "",
+			},
+			11: {
+				"AppName": "Sling-Employee",
+				"AppType": "企业监控软件",
+				"AppRank": "12",
+				"AppLink": "",
+			},
+			12: {
+				"AppName": "Humanity-Employee",
+				"AppType": "企业监控软件",
+				"AppRank": "13",
+				"AppLink": "",
+			},
+			13: {
+				"AppName": "Deputy-Shift",
+				"AppType": "企业监控软件",
+				"AppRank": "14",
+				"AppLink": "",
+			},
+			14: {
+				"AppName": "Employee Link-Timesheet",
+				"AppType": "企业监控软件",
+				"AppRank": "15",
+				"AppLink": "",
+			},
+			15: {
+				"AppName": "busybusy",
+				"AppType": "企业监控软件",
+				"AppRank": "16",
+				"AppLink": "",
+			},
+			////////////////////////////////////////监控员工软件结束////////////////////////////////////////
 		},
 		3: {
+			////////////////////////////////////////违规收集软件开始////////////////////////////////////////
 			0: {
 				"AppName": "爱卡汽车",
 				"AppType": "违规收集软件",
@@ -209,20 +364,203 @@ func GetPreventList(c *gin.Context) {
 				"AppRank": "1",
 				"AppLink": "",
 			},
+			////////////////////////////////////////违规收集软件结束////////////////////////////////////////
 		},
 		4: {
+			////////////////////////////////////////大数据滥收集软件开始////////////////////////////////////////
 			0: {
-				"AppName": "美团",
+				"AppName": "Google统计",
 				"AppType": "大数据滥收集软件",
-				"AppRank": "1",
+				"AppRank": "0",
 				"AppLink": "",
 			},
 			1: {
-				"AppName": "淘宝",
+				"AppName": "友盟统计",
 				"AppType": "大数据滥收集软件",
-				"AppRank": "1",
+				"AppRank": "0",
 				"AppLink": "",
 			},
+			2: {
+				"AppName": "极光统计",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			3: {
+				"AppName": "MobTech",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			4: {
+				"AppName": "MobTech",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			5: {
+				"AppName": "TalkingData",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			6: {
+				"AppName": "阿里妈妈",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			7: {
+				"AppName": "字节跳动",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			8: {
+				"AppName": "快手",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			9: {
+				"AppName": "百度",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			10: {
+				"AppName": "腾讯",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			11: {
+				"AppName": "美团",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			12: {
+				"AppName": "淘宝",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			13: {
+				"AppName": "今日头条",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			14: {
+				"AppName": "苏宁易购",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			15: {
+				"AppName": "真快乐",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			16: {
+				"AppName": "京东",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			17: {
+				"AppName": "拼多多",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			18: {
+				"AppName": "58同城",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			19: {
+				"AppName": "携程旅行",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			20: {
+				"AppName": "同程旅行",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			21: {
+				"AppName": "知乎",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			22: {
+				"AppName": "新浪微博",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			23: {
+				"AppName": "唯品会",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			24: {
+				"AppName": "得物",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			25: {
+				"AppName": "Bilibili",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			26: {
+				"AppName": "西瓜视频",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			27: {
+				"AppName": "小红书",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			28: {
+				"AppName": "探探",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			29: {
+				"AppName": "陌陌",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			30: {
+				"AppName": "Soul",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			31: {
+				"AppName": "链家",
+				"AppType": "大数据滥收集软件",
+				"AppRank": "0",
+				"AppLink": "",
+			},
+			////////////////////////////////////////大数据滥收集软件结束////////////////////////////////////////
 		},
 	}
 	app.OkWithData(s[req.PrevemtType], c)
